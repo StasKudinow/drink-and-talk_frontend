9fe5ba8bb05a738148e444e26fd8bd7ff2a8a539
Taisto Seppanen <taistopetrseppanen@gmail.com>
2023-03-29 20:06:46 +0200

    reafactor

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,9 +7,8 @@ function Button({
   url,
   onOpenMenu,
   isMenuOpen,
-  onRefreshPage
+  onRefreshPage,
 }) {
-
   const history = useHistory()
 
   // const handleClick =
@@ -37,71 +36,70 @@ function Button({
   // тут обработка другух вариантов использования кнопки
 
   let buttonClassName
-  if (variant === 'black-button') {
-    buttonClassName = (
-      `
-        w-70
-        h-15
-        bg-black
-        font-normal
-        text-h3-web
-        leading-7
-        text-white
-        rounded-default
-        duration-300
-        z-10
-        active:shadow-button-shadow
-        hover:bg-opacity-10
-        hover:text-black
-      `
-    )
-  } else if (variant === 'orange-button') {
-    buttonClassName = (
-      `
-        w-70
-        h-15
-        bg-orange
-        font-normal
-        text-h3-web
-        leading-7
-        text-white
-        rounded-default
-        duration-300
-        z-10
-        active:shadow-button-shadow
-        hover:bg-opacity-10
-        hover:text-black
-      `
-    )
-  } else if (variant === 'green-button') {
-    buttonClassName = (
-      `
-        w-70
-        h-15
-        bg-green
-        font-normal
-        text-h3-web
-        leading-7
-        text-black
-        rounded-default
-        duration-300
-        z-10
-        active:shadow-button-shadow
-        hover:bg-opacity-10
-        hover:text-black
-      `
-    )
-  } else if (variant === 'drop-down-button') {
-    buttonClassName = (
-      `
-        w-6
-        h-3
-        bg-profile_button
-        bg-cover
-        duration-300
-        ${isMenuOpen ? 'rotate-180' : ''}
-      `
-    )
+  switch (variant) {
+    case 'black-button':
+      buttonClassName = `
+          w-70
+          h-15
+          bg-black
+          font-normal
+          text-h3-web
+          leading-7
+          text-white
+          rounded-default
+          duration-300
+          z-10
+          active:shadow-button-shadow
+          hover:bg-opacity-10
+          hover:text-black
+        `
+      break
+    case 'orange-button':
+      buttonClassName = `
+          w-70
+          h-15
+          bg-orange
+          font-normal
+          text-h3-web
+          leading-7
+          text-white
+          rounded-default
+          duration-300
+          z-10
+          active:shadow-button-shadow
+          hover:bg-opacity-10
+          hover:text-black
+        `
+      break
+    case 'green-button':
+      buttonClassName = `
+          w-70
+          h-15
+          bg-green
+          font-normal
+          text-h3-web
+          leading-7
+          text-black
+          rounded-default
+          duration-300
+          z-10
+          active:shadow-button-shadow
+          hover:bg-opacity-10
+          hover:text-black
+        `
+      break
+    case 'drop-down-button':
+      buttonClassName = `
+          w-6
+          h-3
+          bg-profile_button
+          bg-cover
+          duration-300
+          ${isMenuOpen ? 'rotate-180' : ''}
+        `
+      break
+    default:
+      throw new Error('Button variant not found')
   }
 
   return (
