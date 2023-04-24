bfea6b553e8a0163963afa9a489e3c626d7aadec
Ganiev <ganievhushbaht@yandex.ru>
2023-04-24 12:43:33 +0300

    feat: added microphone button

--- src/components/VideoBlock.js
+++ src/components/VideoBlock.js
@@ -1,7 +1,27 @@
 import VideoController from '../controllers/VideoController.ts'
 import React, { useEffect, useState } from 'react'
 
+import micOn from '../images/icons/micOn.svg'
+import micOff from '../images/icons/micOff.svg'
+
 const stream = new VideoController(1920, 1080)
+
+function MicToggle() {
+  const [isMicOn, setIsMicOn] = useState(true)
+
+  const toggleMic = () => {
+    setIsMicOn(!isMicOn)
+  }
+
+  return (
+    <img
+      className="absolute top-95 left-110"
+      src={isMicOn ? micOn : micOff}
+      alt="mic"
+      onClick={toggleMic}
+    />
+  )
+}
 
 export default function VideoBlock() {
   const [error, setError] = useState(null)
@@ -37,7 +57,7 @@ export default function VideoBlock() {
       <video
         autoPlay
         playsInline
-        className="w-full h-full border"></video>
+        className="w-full h-full border relative"></video>
       {error && <div className="text-red-500">{error}</div>}
       <button
         onClick={() => {
@@ -52,10 +72,11 @@ export default function VideoBlock() {
         Click button 1000
       </button>
       <button
+        className=""
         onClick={() => {
           stream.muteToggler()
         }}>
-        mute/onmute
+        <MicToggle className="" />
       </button>
     </div>
   )
